84441e2827b29f54df2e6e4ec19608240d2a558d
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-02-13 14:45:25 +0530

    Port SPM_hrf to Python

--- python/spm.py
+++ python/spm.py
@@ -53,3 +53,36 @@ def spm_orth(X,OPT='pad'):
         return gs(X,row_vecs=False,norm=False)
     else:
         return X
+
+def spm_hrf(RT, P=None, fMRI_T=16):
+    """
+    @RT - scan repeat time
+    @P  - parameters of the response function (two gamma functions)
+
+    defaults  (seconds)
+    %	P[0] - Delay of Response (relative to onset)	    6
+    %	P[1] - Delay of Undershoot (relative to onset)     16
+    %	P[2] - Dispersion of Response			            1
+    %	P[3] - Dispersion of Undershoot			            1
+    %	P[4] - Ratio of Response to Undershoot		        6
+    %	P[5] - Onset (seconds)				                0
+    %	P[6] - Length of Kernel (seconds)		           32
+
+    hrf  - hemodynamic response function
+    P    - parameters of the response function
+    """
+    p = np.array([6, 16, 1, 1, 6, 0, 32], dtype=float)
+    if P is not None:
+        p[0:len(P)] = P
+    _spm_Gpdf = lambda x, h, l: np.exp(h * np.log(l) + (h - 1) * np.log(x) - (l * x) - gammaln(h))
+    # modelled hemodynamic response function - {mixture of Gammas}
+    dt = RT / float(fMRI_T)
+    u = np.arange(0, int(p[6] / dt + 1)) - p[5] / dt
+    with np.errstate(divide='ignore'):  # Known division-by-zero
+        hrf = _spm_Gpdf(u, p[0] / p[2], dt / p[2]) - _spm_Gpdf(u, p[1] / p[3],
+                                                               dt / p[3]) / p[4]
+    idx = np.arange(0, int((p[6] / RT) + 1)) * fMRI_T
+    hrf = hrf[idx]
+    hrf = np.nan_to_num(hrf)
+    hrf = hrf / np.sum(hrf)
+    return hrf
