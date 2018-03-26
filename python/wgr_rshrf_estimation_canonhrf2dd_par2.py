d6c6bdaa1960abc045566d6e6211762c40d8ea9d
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-03-26 11:27:15 +0530

    Translate wgr_glsco to Python

--- python/wgr_rshrf_estimation_canonhrf2dd_par2.py
+++ python/wgr_rshrf_estimation_canonhrf2dd_par2.py
@@ -22,3 +22,53 @@ def wgr_onset_design(u,bf,T,T0,nscans):
     """
     X = X[(np.arange(0,nscans)*T)+(T0-1),:]
     return X
+
+def wgr_glsco(X,Y,AR_lag=1,max_iter=20):
+    """
+    Linear regression when disturbance terms follow AR(p)
+    -----------------------------------
+    Model:
+    Yt = Xt * Beta + ut ,
+    ut = Phi1 * u(t-1) + ... + Phip * u(t-p) + et
+    where et ~ N(0,s^2)
+    -----------------------------------
+    Algorithm:
+    Cochrane-Orcutt iterated regression (Feasible generalized least squares)
+    -----------------------------------
+    Usage:
+    Y = dependent variable (n * 1 vector)
+    X = regressors (n * k matrix)
+    AR_lag = number of lags in AR process
+    -----------------------------------
+    Returns:
+    Beta = estimator corresponding to the k regressors
+    """
+    nobs, nvar = X.shape
+    Beta = np.linalg.lstsq(X,Y)[0]
+    resid = Y - (X.dot(Beta))
+
+    max_tol = min(1e-6,max(np.absolute(Beta))/1000)
+    for r in range(max_iter):
+        Beta_temp = Beta
+        X_AR = np.zeros((nobs-(2*AR_lag),AR_lag))
+
+        for m in range(AR_lag):
+            X_AR[:,m] = resid[AR_lag-m-1:nobs-AR_lag-m-1]
+
+        Y_AR = resid[AR_lag:nobs-AR_lag]
+        AR_para = np.linalg.lstsq(X_AR,Y_AR)[0]
+
+        X_main = X[AR_lag:nobs,:]
+        Y_main = Y[AR_lag:nobs]
+
+        for m in range(AR_lag):
+            X_main = X_main - (AR_para[m]*(X[AR_lag-m-1:nobs-m-1,:]))
+            Y_main = Y_main - (AR_para[m]*(Y[AR_lag-m-1:nobs-m-1]))
+
+        Beta = np.linalg.lstsq(X_main,Y_main)[0]
+        resid = Y[AR_lag:nobs] - X[AR_lag:nobs,:].dot(Beta)
+        if(max(np.absolute(Beta - Beta_temp))<max_tol):
+            break
+
+    res_sum = np.sum(resid**2)
+    return res_sum, Beta
