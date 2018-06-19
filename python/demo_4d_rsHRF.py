374ae3e31f22bc00aec65e20226377d88261d58a
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-06-19 16:31:08 +0530

    Fix Plotting of the 2nd graph Add X-Axis Labels along with TR values for X-Axis Add options for canon2dd, sFIR and FIR Save event-numbers is a separate .nii Apply filtering and zscore before plotting as well

--- python/demo_4d_rsHRF.py
+++ python/demo_4d_rsHRF.py
@@ -3,8 +3,11 @@ import numpy as np
 import os
 from wgr_rshrf_estimation_canonhrf2dd_par2 import *
 from wgr_get_parameters import *
+from wgr_rsHRF_FIR import *
+from rest_IdealFilter import *
 import matplotlib.pyplot as plt
 from scipy import stats, signal
+from scipy.sparse import lil_matrix
 import scipy.io as sio
 
 """
@@ -22,10 +25,14 @@ PARAMETERS
 """
 temporal_mask = []
 
-TR = 2
+TR = 1.5
 para = {}
 
+para['estimation'] = 'sFIR'
+
 para['TR'] = TR
+
+para['passband'] = [0.01, 0.08]
 
 para['T'] = 3
 
@@ -33,6 +40,9 @@ para['T0'] = 3
 
 if para['T'] == 1:
     para['T0'] = 1
+
+min_onset_search = 4
+max_onset_search = 8
 
 para['dt'] = para['TR'] / para['T']
 
@@ -44,7 +54,7 @@ para['thr'] = 1
 
 para['len'] = 24
 
-para['lag'] = np.arange(np.fix(3/para['dt']),np.fix(9/para['dt'])+1,dtype='int')
+para['lag'] = np.arange(np.fix(min_onset_search/para['dt']),np.fix(max_onset_search/para['dt'])+1,dtype='int')
 
 """
 fMRI Data
@@ -75,14 +85,19 @@ for isub in range(len(sub)):
         data = v1.get_data()
         nobs = data.shape[3]
         data1 = np.reshape(data, (-1, nobs), order='F').T
-        bold_sig = data1[:, voxel_ind]
+        bold_sig = stats.zscore(data1[:, voxel_ind],ddof=1)
+        bold_sig = rest_IdealFilter(bold_sig,para['TR'],para['passband'])
         data_deconv = np.zeros(bold_sig.shape)
+        event_number = np.zeros((1,bold_sig.shape[1]))
 
         print('Retrieving HRF ...')
-        beta_hrf, bf, event_bold = wgr_rshrf_estimation_canonhrf2dd_par2(bold_sig,para,temporal_mask)
 
-        hrfa = np.dot(bf,beta_hrf[np.arange(0,bf.shape[1]),:])
-        hrf1 = hrfa[:,0]
+        if 'canon' in para['estimation']:
+            beta_hrf, bf, event_bold = wgr_rshrf_estimation_canonhrf2dd_par2(bold_sig,para,temporal_mask)
+            hrfa = np.dot(bf,beta_hrf[np.arange(0,bf.shape[1]),:])
+        elif 'FIR' in para['estimation']:
+            para['T'] = 1
+            hrfa, event_bold = wgr_rsHRF_FIR(bold_sig,para,temporal_mask)
 
         nvar = hrfa.shape[1]
         PARA = np.zeros((3,nvar))
@@ -107,6 +122,7 @@ for isub in range(len(sub)):
             H = np.fft.fft(np.append(hrf,np.zeros((nobs-max(hrf.shape),1))),axis=0)
             M = np.fft.fft(bold_sig[:,voxel_id])
             data_deconv[:,voxel_id] = np.fft.ifft(H.conj()*M/(H*H.conj()+2))
+            event_number[:,voxel_id] = np.amax(event_bold[voxel_id].shape)
 
         sio.savemat(os.path.join(sub_save_dir,name+'_hrf.mat'),{'para':para,'hrfa':hrfa,'event_bold':event_bold,'PARA':PARA})
         HRF_para_str = ['Height.nii','Time2peak.nii','FWHM.nii']
@@ -119,6 +135,12 @@ for isub in range(len(sub)):
             spm_write_vol(v,data,fname)
             data = data.flatten(order='F')
 
+        fname = os.path.join(sub_save_dir, name + '_event_number.nii')
+        data[voxel_ind] = event_number
+        data = data.reshape(v.get_data().shape, order='F')
+        spm_write_vol(v,data,fname)
+        data = data.flatten(order='F')
+
         data = np.zeros(v1.get_data().shape)
         dat3 = np.zeros(v1.header.get_data_shape()[:-1]).flatten(order='F')
         for i in range(nobs):
@@ -129,14 +151,20 @@ for isub in range(len(sub)):
             dat3 = dat3.flatten(order='F')
         spm_write_vol(v1, data, fname)
 
-        plt.plot(hrfa[:,0],linewidth=1)
+        event_plot = lil_matrix((1,nobs))
+        event_plot[:,event_bold[0]] = 1
+        event_plot = np.ravel(event_plot.toarray())
+
+        plt.plot(TR*np.arange(1,np.amax(hrfa[:,0].shape)+1),hrfa[:,0],linewidth=1)
+        plt.xlabel('time (s)')
         plt.show()
 
-        plt.plot(stats.zscore(bold_sig[:,0],ddof=1),linewidth=1)
-        plt.plot(stats.zscore(data_deconv[:,0],ddof=1),color='r',linewidth=1)
-        markerline, stemlines, baseline = plt.stem(event_bold[0].toarray()[0])
+        plt.plot(TR*np.arange(1,nobs+1),stats.zscore(bold_sig[:,0],ddof=1),linewidth=1)
+        plt.plot(TR*np.arange(1,nobs+1),stats.zscore(data_deconv[:,0],ddof=1),color='r',linewidth=1)
+        markerline, stemlines, baseline = plt.stem(TR*np.arange(1,nobs+1),event_plot)
         plt.setp(baseline,'color','k','markersize',1)
         plt.setp(stemlines,'color','k','markersize',1)
         plt.setp(markerline,'color','k','markersize',3,'marker','d')
         plt.legend(['BOLD','deconvolved','events'])
+        plt.xlabel('time (s)')
         plt.show()
