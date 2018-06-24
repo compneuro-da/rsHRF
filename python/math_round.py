62813ac0df67ebfccb08d7f28455b68b9856b1ce
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-06-24 17:40:26 +0530

    Refactor to meet PEP8 Standards

--- python/math_round.py
+++ python/math_round.py
@@ -1,10 +1,11 @@
 import math
 import numpy as np
 
+
 def np_round_up(x):
     if type(x) is np.ndarray:
         floor_mask = x - np.floor(x) < 0.5
-        z = np.where(floor_mask,np.floor(x),np.ceil(x))
+        z = np.where(floor_mask, np.floor(x), np.ceil(x))
         return z.astype(dtype=np.int)
     else:
         if x - math.floor(x) < 0.5:
