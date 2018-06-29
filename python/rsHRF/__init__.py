40e3e1eb1dc62db99ec98d7202e8312cd8e790b2
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-06-29 10:18:58 +0530

    Set-Up Directory Structure for Packages Implement Basic Argument / Parameter Design Handle Errors pertaining to inconsistent data / format

---
+++ python/rsHRF/__init__.py
@@ -0,0 +1,8 @@
+import rsHRF.spm_dep
+import rsHRF.processing
+import rsHRF.canon
+import rsHRF.sFIR
+import rsHRF.parameters
+import rsHRF.fourD_rsHRF
+__all__ = ["spm_dep", "processing", "canon",
+           "sFIR", "parameters", "fourD_rsHRF.py"]
