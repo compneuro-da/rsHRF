c01c2bdd38ff9c45730a13ccc232ccf9f110a783
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-07-17 17:56:01 +0530

    Implement Participant Level Analysis
    Add options for analysis-level and
    --participant_label. The latter is used to
    select specific subjects for whom the
    analysis is to be performed.

--- python/rsHRF/CLI.py
+++ python/rsHRF/CLI.py
@@ -4,11 +4,11 @@ from bids.grabbids import BIDSLayout
 import numpy as np
 from rsHRF import spm_dep, fourD_rsHRF
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 def get_parser():
     parser = ArgumentParser(description='retrieves the onsets of pseudo-events triggering a '
-                                        'hemodynamic response from resting state fMRI BOLD '
+                                        'haemodynamic response from resting state fMRI BOLD '
                                         'voxel-wise signal')
 
     group_input = parser.add_mutually_exclusive_group(required=True)
@@ -17,13 +17,24 @@ def get_parser():
                              help='the absolute path to a single data file')
 
     group_input.add_argument('bids_dir', nargs='?', action='store', type=op.abspath,
-                            help='the root folder of a BIDS valid dataset '
-                                 '(sub-XXXXX folders should be found at the '
-                                 'top level in this folder).')
+                             help='the root folder of a BIDS valid dataset '
+                                  '(sub-XXXXX folders should be found at the '
+                                  'top level in this folder).')
 
     parser.add_argument('output_dir', action='store', type=op.abspath,
-                        help='the output path for the outcomes of preprocessing and visual '
-                             'reports')
+                        help='the output path for the outcomes of processing')
+
+    parser.add_argument('analysis_level', help='Level of the analysis that will be performed. '
+                        'Multiple participant level analyses can be run independently '
+                        '(in parallel) using the same output_dir.', choices=['participant', 'group'], nargs='?')
+
+    parser.add_argument('--participant_label',
+                        help='The label(s) of the participant(s) that should be analyzed. The label '
+                             'corresponds to sub-<participant_label> from the BIDS spec '
+                             '(so it does not include "sub-"). If this parameter is not '
+                             'provided all subjects should be analyzed. Multiple '
+                             'participants can be specified with a space separated list.',
+                        nargs="+")
 
     group_mask = parser.add_mutually_exclusive_group(required=True)
 
@@ -37,45 +48,44 @@ def get_parser():
     group_para = parser.add_argument_group('Parameters')
 
     group_para.add_argument('--estimation', action='store',
-                        choices=['canon2dd', 'sFIR', 'FIR'], required=True,
-                        help='Choose the estimation procedure from '
-                             'canon2dd (canonical shape with 2 derivatives), '
-                             'sFIR (smoothed Finite Impulse Response) , '
-                             'FIR (Finite Impulse Response)')
+                            choices=['canon2dd', 'sFIR', 'FIR'], required=True,
+                            help='Choose the estimation procedure from '
+                                 'canon2dd (canonical shape with 2 derivatives), '
+                                 'sFIR (smoothed Finite Impulse Response) , '
+                                 'FIR (Finite Impulse Response)')
 
     group_para.add_argument('--passband', action='store', type=float, nargs=2, metavar=('LOW_FREQ','HIGH_FREQ'),
-                           default=[0.01, 0.08],
-                           help='set intervals for bandpass filter, default is 0.01 - 0.08')
+                            default=[0.01, 0.08],
+                            help='set intervals for bandpass filter, default is 0.01 - 0.08')
 
     group_para.add_argument('-T', action='store', type=float, nargs=1, default=3,
-                        help='set T parameter')
+                            help='set T parameter')
 
     group_para.add_argument('-T0', action='store', type=float, nargs=1, default=3,
-                        help='set T0 parameter')
+                            help='set T0 parameter')
 
     group_para.add_argument('-TD_DD', action='store', type=int, nargs=1, default=2,
-                        help='set TD_DD parameter')
+                            help='set TD_DD parameter')
 
     group_para.add_argument('-AR_lag', action='store', type=float, nargs=1, default=1,
-                        help='set AR_lag parameter')
+                            help='set AR_lag parameter')
 
     group_para.add_argument('--thr', action='store', type=float, nargs=1, default=1,
-                        help='set thr parameter')
+                            help='set thr parameter')
 
     group_para.add_argument('--len', action='store', type=int, nargs=1, default=24,
-                        help='set len parameter')
+                            help='set len parameter')
 
     group_para.add_argument('--min_onset_search', action='store', type=int, nargs=1, default=4,
-                        help='set min_onset_search parameter')
+                            help='set min_onset_search parameter')
 
     group_para.add_argument('--max_onset_search', action='store', type=int, nargs=1, default=8,
-                        help='set max_onset_search parameter')
+                            help='set max_onset_search parameter')
 
     return parser
 
 
 def run_rsHRF():
-
     parser = get_parser()
 
     args = parser.parse_args()
@@ -88,8 +98,17 @@ def run_rsHRF():
 
     para = arg_groups['Parameters']
 
+    if args.input_file is not None and args.analysis_level:
+        parser.error('analysis_level cannot be used with --input_file, do not supply it')
+
+    if args.input_file is not None and args.participant_label:
+        parser.error('participant_labels are not to be used with --input_file, do not supply it')
+
     if args.input_file is not None and args.brainmask:
         parser.error('--brainmask cannot be used with --input_file, use --atlas instead')
+
+    if args.bids_dir is not None and not args.analysis_level:
+        parser.error('analysis_level needs to be supplied with bids_dir, choices=[participant, group]')
 
     if args.input_file is not None and (not args.input_file.endswith(('.nii', '.nii.gz'))):
         parser.error('--input_file should end with .nii or .nii.gz')
@@ -110,7 +129,19 @@ def run_rsHRF():
     if args.bids_dir is not None and args.atlas is not None:
         # carry analysis with bids_dir and 1 atlas
         layout = BIDSLayout(args.bids_dir)
-        all_inputs = layout.get(modality='func', task='rest', type='preproc', extensions=['nii', 'nii.gz'])
+
+        if args.participant_label:
+            input_subjects = args.participant_label
+            subjects_to_analyze = layout.get_subjects(subject=input_subjects)
+        else:
+            subjects_to_analyze = layout.get_subjects()
+
+        if not subjects_to_analyze:
+            parser.error('Could not find participants. Please make sure the BIDS data '
+                         'structure is present and correct. Datasets can be validated online '
+                         'using the BIDS Validator (http://incf.github.io/bids-validator/).')
+
+        all_inputs = layout.get(modality='func', subject=subjects_to_analyze, task='rest', type='preproc', extensions=['nii', 'nii.gz'])
         if not all_inputs != []:
             parser.error('There are no files of type *preproc.nii / *preproc.nii.gz '
                          'Please make sure to have at least one file of the above type '
@@ -129,9 +160,22 @@ def run_rsHRF():
                 fourD_rsHRF.demo_4d_rsHRF(all_inputs[file_count], args.atlas, args.output_dir, para, mode='bids w/ atlas')
 
     if args.bids_dir is not None and args.brainmask:
+        # carry analysis with bids_dir and brainmask
         layout = BIDSLayout(args.bids_dir)
-        all_inputs = layout.get(modality='func', task='rest', type='preproc', extensions=['nii', 'nii.gz'])
-        all_masks = layout.get(modality='func', task='rest', type='brainmask', extensions=['nii', 'nii.gz'])
+
+        if args.participant_label:
+            input_subjects = args.participant_label
+            subjects_to_analyze = layout.get_subjects(subject=input_subjects)
+        else:
+            subjects_to_analyze = layout.get_subjects()
+
+        if not subjects_to_analyze:
+            parser.error('Could not find participants. Please make sure the BIDS data '
+                         'structure is present and correct. Datasets can be validated online '
+                         'using the BIDS Validator (http://incf.github.io/bids-validator/).')
+
+        all_inputs = layout.get(modality='func', subject=subjects_to_analyze, task='rest', type='preproc', extensions=['nii', 'nii.gz'])
+        all_masks = layout.get(modality='func', subject=subjects_to_analyze, task='rest', type='brainmask', extensions=['nii', 'nii.gz'])
 
         if not all_inputs != []:
             parser.error('There are no files of type *preproc.nii / *preproc.nii.gz '
